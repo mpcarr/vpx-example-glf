cd80b5351ac55b06f6414482c7b72f752258c9ff
flux <mpcarr84@gmail.com>
2025-06-22 17:08:45 +0100

    update watcher to create dest folder

--- scripts/Gruntfile.js
+++ scripts/Gruntfile.js
@@ -1,8 +1,18 @@
 module.exports = function (grunt) {
   grunt.initConfig({
+    clean: {
+      dest: ['dest/'] // Cleans the dest folder before recreating it
+    },
+    mkdir: {
+      dest: {
+        options: {
+          create: ['dest/vpx'] // Creates the dest/vpx folder
+        },
+      },
+    },
     concat: {
       vpx: {
-        src: ['src/vpx/**/*.vbs', 'src/game/**/*.vbs', 'src/glf/glf.vbs', 'src/glf/shows/*.vbs', '!src/glf/yamlshows/*.yaml', '!src/unittests/**/*.vbs', '!src/**/*.test.vbs', '!src/**/*-mpf.vbs','!src/**/*-ignore.vbs'],
+        src: ['src/vpx/**/*.vbs', 'src/game/**/*.vbs', 'src/glf/glf.vbs', 'src/glf/shows/*.vbs', '!src/glf/yamlshows/*.yaml', '!src/unittests/**/*.vbs', '!src/**/*.test.vbs', '!src/**/*-mpf.vbs', '!src/**/*-ignore.vbs'],
         dest: 'dest/vpx/tablescript.vbs',
       },
     },
@@ -15,12 +25,13 @@ module.exports = function (grunt) {
         }
       },
     }
-
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  // Default task(s).
-  grunt.registerTask('default', ['concat']);
+  grunt.loadNpmTasks('grunt-contrib-clean'); // Load the clean task
+  grunt.loadNpmTasks('grunt-mkdir');       // Load the mkdir task
 
+  // Default task(s).
+  grunt.registerTask('default', ['clean:dest', 'mkdir:dest', 'concat']); // Run clean and mkdir before concat
 };
